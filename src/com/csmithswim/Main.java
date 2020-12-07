7aa46c5f1102c1370027b2b1bc0193e9acbbecbb
Cullen S <cullensmi@gmail.com>
2020-12-07 16:59:15 -0500

    Solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -25,6 +25,36 @@ public class Main {
             }
         }
 
+        //Arrays 0 - 4 is the first element of the hourglass
+        //9 non zero elements to make an hour glass
+
+        ArrayList<Integer> hourGlassSums = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+//                if (arr[i][j] != 0 && arr[i][j+1] != 0 && arr[i][j+2]!=0
+//                    && arr[i+1][j+1] != 0
+//                    && arr[i+2][j] != 0 && arr[i+2][j+1] != 0 && arr[i+2][j+2]!=0) {
+
+                    hourGlassSums.add(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
+
+//                }
+            }
+        }
+        Collections.sort(hourGlassSums);
+//        System.out.println(hourGlassSums);
+        System.out.println(hourGlassSums.get(hourGlassSums.size()-1));
+
+
+
         scanner.close();
     }
 }
+
+/*
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 9 2 -4 -4 0
+0 0 0 -2 0 0
+0 0 -1 -2 -4 0
+ */
